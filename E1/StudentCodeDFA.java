de0600599153cb8112e900ecab2966152fe69e3c
Mina Sharifi <mn.sharifi19@gmail.com>
2024-09-09 14:07:27 +0200

    Add files via upload

---
+++ E1/StudentCodeDFA.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class StudentCodeDFA {
+    public static boolean result(String s) {
+        int state = 0;
+        int i = 0;
+        while (state >= 0 && i < s.length()) {
+            final char ch = s.charAt(i++);
+            switch (state) {
+                case 0:
+                    if (Character.isDigit(ch) && (ch % 2 != 0))
+                        state = 1;
+                    else if (Character.isDigit(ch) && (ch % 2 == 0))
+                        state = 2;
+                    else if (Character.isLetter(ch))
+                        state = -1;
+                    else
+                        state = -1;
+                    break;
+
+                case 1:
+                    if (Character.isDigit(ch) && (ch % 2 != 0))
+                        state = 1;
+                    else if (Character.isDigit(ch) && (ch % 2 == 0))
+                        state = 2;
+                    else if (Character.isLetter(ch) && (ch >= 'L' && ch <= 'Z'))
+                        state = 3;
+                    else if (Character.isLetter(ch) && (ch >= 'A' && ch <= 'K'))
+                        state = -1;
+                    else
+                        state = -1;
+
+                    break;
+                case 2:
+                    if (Character.isDigit(ch) && (ch % 2 != 0))
+                        state = 1;
+                    else if (Character.isDigit(ch) && (ch % 2 == 0))
+                        state = 2;
+                    else if (Character.isLetter(ch) && (ch >= 'A' && ch <= 'K'))
+                        state = 3;
+                    else if (Character.isLetter(ch) && (ch >= 'L' && ch <= 'Z'))
+                        state = -1;
+                    else
+                        state = -1;
+                    break;
+
+                case 3:
+
+                    if (Character.isDigit(ch))
+                        state = -1;
+                    else if (Character.isLetter(ch))
+                        state = 3;
+                    else
+                        state = -1;
+
+            }
+        }
+        return state == 3;
+        // return state != 3 && state >= 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter a Sequence: ");
+
+        String input = sc.nextLine();
+
+        System.out.println(result(input) ? "OK" : "NOPE");
+
+        sc.close();
+    }
+}
