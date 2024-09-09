ee5628083045ea27f74affb9769561895f2ff47e
Mina Sharifi <mn.sharifi19@gmail.com>
2024-09-09 14:17:56 +0200

    Create NumberTok.java

---
+++ E2/E2/NumberTok.java
@@ -0,0 +1,17 @@
+package E2;
+
+public class NumberTok extends Token {
+    public int value; // Assuming the value of the number is an integer
+
+    public NumberTok(int tag, int value) {
+        super(tag);
+        this.value = value;
+    }
+
+    public String toString() {
+        return "<" + tag + ", " + value + ">";
+    }
+
+    public static final NumberTok num = new NumberTok(Tag.NUM, 0);
+
+}
